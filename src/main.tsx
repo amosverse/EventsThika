88189b2f6178f82bbeccfaafeceb93295d844c61
Amos-mathu <amos.mathu.0@gmail.com>
2026-02-05 22:27:34 +0300

    checking..

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
-import { CartProvider } from './context/CartContext'
+import maintenanceImage from './assets/maintainance.jpg'
+
+function MaintenancePage() {
+  return (
+    <div className="min-h-screen w-full bg-background flex items-center justify-center p-6">
+      <img
+        src={maintenanceImage}
+        alt="We are on maintenance"
+        className="max-h-[80vh] w-auto max-w-full object-contain"
+      />
+    </div>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CartProvider>
-      <RouterProvider router={router} />
-    </CartProvider>
+    <MaintenancePage />
   </StrictMode>,
 )
